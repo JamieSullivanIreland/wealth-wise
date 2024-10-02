80a5c5473c93b0631e254ece026d04d80d89f44c
Jamie Sullivan <jamiesullivan523@gmail.com>
2024-10-02 16:38:57 +0100

    Fixed styling and added dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import Layout from '@/components/Layout/Layout';
 import Dashboard from '@/components/Dashboard/Dashboard';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Wealth Wise',
+  description: 'This is Wealth Wise',
+};
+
 export default function Home() {
   return (
     <>
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -17,7 +17,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: ISidebarProps) => {
   return (
     <ClickOutside onClick={() => setSidebarOpen(false)}>
       <aside
-        className={`left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:translate-x-0 `}
+        className={`fixed left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:translate-x-0 ${
+          sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+        }`}
       >
         <div className='flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5'>
           <Link href='/'>

05a17d5e068125f82a6ce2bbf189112a4dc9cb9f
Jamie Sullivan <jamiesullivan523@gmail.com>
2024-10-04 16:59:34 +0100

    Removed some uneccessary

--- src/components/Containers/SettingsContainer.tsx
+++ src/components/Containers/SettingsContainer.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const SettingsContainer = () => {
   return <div>Settings</div>;
 };
--- src/components/Containers/TablesContainer.tsx
+++ src/components/Containers/TablesContainer.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const TablesContainer = () => {
   return <div>Tables</div>;
 };

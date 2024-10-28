7b800b502286605c924a8ddf198904636a658198
Jamie Sullivan <jamiesullivan523@gmail.com>
2024-10-28 15:47:13 +0000

    Installed next auth and setup auth provider component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import 'jsvectormap/dist/jsvectormap.css';
 import 'flatpickr/dist/flatpickr.min.css';
 import '@/css/satoshi.css';
 import '@/css/style.css';
+import AuthProvider from '@/components/Common/AuthProvider';
 
 export default function RootLayout({
   children,
@@ -12,12 +13,14 @@ export default function RootLayout({
   const loading = false;
 
   return (
-    <html lang='en'>
-      <body suppressHydrationWarning={true}>
-        <div className='dark:bg-boxdark-2 dark:text-bodydark'>
-          {loading ? <div>Loading...</div> : children}
-        </div>
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang='en'>
+        <body suppressHydrationWarning={true}>
+          <div className='dark:bg-boxdark-2 dark:text-bodydark'>
+            {loading ? <div>Loading...</div> : children}
+          </div>
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
---
+++ src/utils/authOptions.ts
@@ -0,0 +1,28 @@
+import NextAuth, { AuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ profile }) {
+      // Sign In
+    },
+    async session({ session }) {
+      // Session
+    },
+  },
+};
+
+export default NextAuth(authOptions);

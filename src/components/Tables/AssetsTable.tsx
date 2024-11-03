ab7d492ceee092444cbc29b5dcda6c83e96f1526
Jamie Sullivan <jamiesullivan523@gmail.com>
2024-11-03 13:30:40 +0000

    Added asset data into table

--- src/components/Tables/AssetsTable.tsx
+++ src/components/Tables/AssetsTable.tsx
@@ -1,100 +1,74 @@
-const productData = [
-  {
-    image: '/images/product/product-01.png',
-    name: 'Apple Watch Series 7',
-    category: 'Electronics',
-    price: 296,
-    sold: 22,
-    profit: 45,
-  },
-  {
-    image: '/images/product/product-02.png',
-    name: 'Macbook Pro M1',
-    category: 'Electronics',
-    price: 546,
-    sold: 12,
-    profit: 125,
-  },
-  {
-    image: '/images/product/product-03.png',
-    name: 'Dell Inspiron 15',
-    category: 'Electronics',
-    price: 443,
-    sold: 64,
-    profit: 247,
-  },
-  {
-    image: '/images/product/product-04.png',
-    name: 'HP Probook 450',
-    category: 'Electronics',
-    price: 499,
-    sold: 72,
-    profit: 103,
-  },
-];
-
 interface IProps {
   assets: IAsset[];
 }
 
 const AssetsTable = ({ assets }: IProps) => {
-  console.log(assets);
+  const nf = new Intl.NumberFormat('en-GB', {
+    style: 'currency',
+    currency: 'EUR',
+  });
+
+  const getPercentage = (value: number, cost: number) => {
+    const diff = value - cost;
+    const percentage = (diff / cost) * 100;
+    return `${percentage.toFixed(0)}%`;
+  };
+
   return (
     <div className='rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark'>
       <div className='px-4 py-6 md:px-6 xl:px-7.5'>
         <h4 className='text-xl font-semibold text-black dark:text-white'>
-          Top Products
+          Assets
         </h4>
       </div>
 
       <div className='grid grid-cols-6 border-t border-stroke px-4 py-4.5 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5'>
         <div className='col-span-3 flex items-center'>
-          <p className='font-medium'>Product Name</p>
+          <p className='font-medium'>Type</p>
         </div>
         <div className='col-span-2 hidden items-center sm:flex'>
-          <p className='font-medium'>Category</p>
+          <p className='font-medium'>Change</p>
         </div>
         <div className='col-span-1 flex items-center'>
-          <p className='font-medium'>Price</p>
+          <p className='font-medium'>Cost</p>
         </div>
         <div className='col-span-1 flex items-center'>
-          <p className='font-medium'>Sold</p>
-        </div>
-        <div className='col-span-1 flex items-center'>
-          <p className='font-medium'>Profit</p>
+          <p className='font-medium'>Value</p>
         </div>
       </div>
 
-      {productData.map((product, key) => (
-        <div
-          className='grid grid-cols-6 border-t border-stroke px-4 py-4.5 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5'
-          key={key}
-        >
-          <div className='col-span-3 flex items-center'>
-            <div className='flex flex-col gap-4 sm:flex-row sm:items-center'>
+      {assets.map((asset: IAsset, i: number) => {
+        const { category_id, cost, value } = asset;
+        return (
+          <div
+            className='grid grid-cols-6 border-t border-stroke px-4 py-4.5 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5'
+            key={i}
+          >
+            <div className='col-span-3 flex items-center'>
+              <div className='flex flex-col gap-4 sm:flex-row sm:items-center'>
+                <p className='text-sm text-black dark:text-white'>
+                  {category_id}
+                </p>
+              </div>
+            </div>
+            <div className='col-span-2 hidden items-center sm:flex'>
               <p className='text-sm text-black dark:text-white'>
-                {product.name}
+                {getPercentage(value, cost)}
+              </p>
+            </div>
+            <div className='col-span-1 flex items-center'>
+              <p className='text-sm text-black dark:text-white'>
+                {nf.format(cost)}
+              </p>
+            </div>
+            <div className='col-span-1 flex items-center'>
+              <p className='text-sm text-black dark:text-white'>
+                {nf.format(value)}
               </p>
             </div>
           </div>
-          <div className='col-span-2 hidden items-center sm:flex'>
-            <p className='text-sm text-black dark:text-white'>
-              {product.category}
-            </p>
-          </div>
-          <div className='col-span-1 flex items-center'>
-            <p className='text-sm text-black dark:text-white'>
-              ${product.price}
-            </p>
-          </div>
-          <div className='col-span-1 flex items-center'>
-            <p className='text-sm text-black dark:text-white'>{product.sold}</p>
-          </div>
-          <div className='col-span-1 flex items-center'>
-            <p className='text-sm text-meta-3'>${product.profit}</p>
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };

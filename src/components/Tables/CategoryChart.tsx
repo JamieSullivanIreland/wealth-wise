278340b1b9f0468bdce25d2b1d6f52c7c6dc1cd4
Jamie Sullivan <jamiesullivan523@gmail.com>
2024-12-20 18:37:55 +0000

    Installed Apex charts and built basic donut chart

--- src/components/Tables/CategoryChart.tsx
+++ src/components/Tables/CategoryChart.tsx
@@ -1,5 +1,38 @@
+'use client';
+
+// import { ApexOptions } from 'apexcharts';
+import React from 'react';
+import dynamic from 'next/dynamic';
+
+const ReactApexChart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+});
+
+const options = {
+  chartOptions: {
+    labels: ['Apple', 'Mango', 'Orange', 'Watermelon'],
+  },
+};
+
 const CategoryChart = () => {
-  return <div className='col-span-4'>CategoryChart</div>;
+  const series = [44, 55, 41, 17, 15];
+
+  return (
+    <div className='col-span-4'>
+      <div
+        id='chartOne'
+        className='-ml-5'
+      >
+        <ReactApexChart
+          options={options}
+          series={series}
+          type='donut'
+          height={350}
+          width={'100%'}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CategoryChart;

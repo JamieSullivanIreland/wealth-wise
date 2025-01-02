270a669aec39840e3be71583c2a58f4fc3c6fcdb
Jamie Sullivan <jamiesullivan523@gmail.com>
2025-01-02 14:00:31 +0000

    Added basic responsive behaviour for dashboard

--- src/components/Containers/DashboardContainer.tsx
+++ src/components/Containers/DashboardContainer.tsx
@@ -13,7 +13,7 @@ interface IProps {
 
 const Dashboard = ({ transactions, assets, categories, networth }: IProps) => {
   const tableClasses =
-    'px-4 py-6 border border-stroke bg-white shadow-default dark:border-strokedark ';
+    'px-4 py-6 sm:px-6 sm:py-8 border border-stroke bg-white shadow-default dark:border-strokedark ';
 
   return (
     <>
@@ -22,7 +22,8 @@ const Dashboard = ({ transactions, assets, categories, networth }: IProps) => {
         btnText='Add'
       />
       <div className='grid grid-rows-auto grid-cols-1 gap-4'>
-        <div className='grid grid-cols-12'>
+        {/* Show above 768px */}
+        <div className='hidden md:grid grid-cols-12'>
           <div
             className={`col-span-8 rounded-s-xl border-r-0 dark:bg-dark-3 ${tableClasses}`}
           >
@@ -34,8 +35,18 @@ const Dashboard = ({ transactions, assets, categories, networth }: IProps) => {
             <CategoryChart categories={categories} />
           </div>
         </div>
-        {/* <div className='grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5'> */}
-        <div className='grid grid-cols-12 gap-4'>
+
+        {/* Show below 768px */}
+        <div className='grid grid-cols-12 md:hidden'>
+          <div
+            className={`col-span-12 rounded-xl border-r-1 dark:bg-dark-3 ${tableClasses}`}
+          >
+            <NetworthTable networth={networth} />
+          </div>
+        </div>
+
+        {/* Show above 1024px */}
+        <div className='hidden lg:grid grid-cols-12 gap-4'>
           <div
             className={`col-span-8 rounded-xl dark:bg-dark-4 ${tableClasses}`}
           >
@@ -43,6 +54,22 @@ const Dashboard = ({ transactions, assets, categories, networth }: IProps) => {
           </div>
           <div
             className={`col-span-4 rounded-xl dark:bg-dark-4 ${tableClasses}`}
+          >
+            <TransactionsTable transactions={transactions} />
+          </div>
+        </div>
+
+        {/* Show below 1024px */}
+        <div className='grid grid-cols-12 gap-4 lg:hidden'>
+          <div
+            className={`col-span-12 rounded-xl dark:bg-dark-4 ${tableClasses}`}
+          >
+            <AssetsTable assets={assets} />
+          </div>
+        </div>
+        <div className='grid grid-cols-12 gap-4 lg:hidden'>
+          <div
+            className={`col-span-12 rounded-xl dark:bg-dark-4 ${tableClasses}`}
           >
             <TransactionsTable transactions={transactions} />
           </div>

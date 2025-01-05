47261391ea100f4da29d655fff9610039a4056a4
Jamie Sullivan <jamiesullivan523@gmail.com>
2025-01-05 14:26:21 +0000

    Added query for cumulated networth for year filter

--- src/app/api/networth/route.ts
+++ src/app/api/networth/route.ts
@@ -39,14 +39,36 @@ export const GET = async (request: NextRequest) => {
   try {
     await connectDB();
 
+    // Original
+    // networth = await Asset.aggregate([
+    //   {
+    //     $match: {
+    //       createdAt: {
+    //         $gt: date,
+    //       },
+    //     },
+    //   },
+    //   {
+    //     $group: {
+    //       _id: {
+    //         [`$${agg}`]: '$createdAt',
+    //       },
+    //       timestamp: { $first: '$createdAt' },
+    //       total: {
+    //         $sum: {
+    //           $subtract: ['$value', '$cost'],
+    //         },
+    //       },
+    //       count: {
+    //         $sum: 1,
+    //       },
+    //     },
+    //   },
+    // ]).sort({ _id: 1 });
+
+    // Working
     networth = await Asset.aggregate([
-      {
-        $match: {
-          createdAt: {
-            $gt: date,
-          },
-        },
-      },
+      // Group results by year and get total networth for each
       {
         $group: {
           _id: {
@@ -58,15 +80,152 @@ export const GET = async (request: NextRequest) => {
               $subtract: ['$value', '$cost'],
             },
           },
-          count: {
-            $sum: 1,
+        },
+      },
+      {
+        $sort: {
+          timestamp: 1,
+        },
+      },
+      {
+        $group: {
+          _id: '',
+          results: {
+            $push: {
+              timestamp: '$timestamp',
+            },
+          },
+          totals: {
+            $push: {
+              $sum: '$total',
+            },
           },
         },
       },
-    ]).sort({ _id: 1 });
+      {
+        $addFields: {
+          result: {
+            $reduce: {
+              input: '$totals',
+              initialValue: {
+                total: null,
+                values: [],
+              },
+              in: {
+                $cond: {
+                  if: {
+                    // No total yet, first iteration
+                    $eq: ['$$value.total', null],
+                  },
+                  then: {
+                    // Set first total and add to array
+                    total: '$$this',
+                    values: {
+                      $concatArrays: ['$$value.values', ['$$this']],
+                    },
+                  },
+                  else: {
+                    $let: {
+                      vars: {
+                        // Add total and current value
+                        cumulatedTotal: {
+                          $add: ['$$value.total', '$$this'],
+                        },
+                      },
+                      in: {
+                        // Set new total and add to array
+                        total: '$$cumulatedTotal',
+                        values: {
+                          $concatArrays: [
+                            '$$value.values',
+                            ['$$cumulatedTotal'],
+                          ],
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      // {
+      //   $project: {
+      //     _id: 0,
+      //     totalNetworth: '$result.total',
+      //     results: '$results',
+      //     values: {
+      //       $concatArrays: ['$results', '$result.values'],
+      //     },
+
+      //   },
+      // },
+    ]);
+
+    // This is for all filters except 'All'
+    // networth = await Asset.aggregate([
+    //   {
+    //     $facet: {
+    //       totalNetworth: [
+    //         {
+    //           $match: {
+    //             createdAt: {
+    //               $gt: date,
+    //             },
+    //           },
+    //         },
+    //         {
+    //           $group: {
+    //             _id: null,
+    //             value: {
+    //               $sum: {
+    //                 $subtract: ['$value', '$cost'],
+    //               },
+    //             },
+    //           },
+    //         },
+    //       ],
+    //       results: [
+    //         {
+    //           $match: {
+    //             createdAt: {
+    //               $gt: date,
+    //             },
+    //           },
+    //         },
+    //         {
+    //           $group: {
+    //             _id: {
+    //               [`$${agg}`]: '$createdAt',
+    //             },
+    //             timestamp: { $first: '$createdAt' },
+    //             total: {
+    //               $sum: {
+    //                 $subtract: ['$value', '$cost'],
+    //               },
+    //             },
+    //             count: {
+    //               $sum: 1,
+    //             },
+    //           },
+    //         },
+    //       ],
+    //     },
+    //   },
+    //   { $unwind: '$totalNetworth' },
+    //   {
+    //     $project: {
+    //       _id: 0,
+    //       total: '$totalNetworth.value',
+    //       results: '$results',
+    //     },
+    //   },
+    // ]);
 
     return new Response(JSON.stringify({ networth }), { status: 200 });
   } catch (error) {
+    console.log(error);
     return new Response('Something went wrong', { status: 500 });
   }
 };

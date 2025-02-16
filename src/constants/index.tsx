6cee554099bdbb7fd2dd4e6f193888dbfa5a42e1
Jamie Sullivan <jamiesullivan523@gmail.com>
2025-02-16 11:30:24 +0000

    Added menu items for assets and transactions

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,9 +1,10 @@
 import {
   faArrowRightFromBracket,
-  faGear,
   faChartPie,
   faTable,
   faBorderAll,
+  faExchange,
+  faMoneyBill1Wave,
 } from '@fortawesome/free-solid-svg-icons';
 
 import Icon from '@/components/Common/Icon';
@@ -23,6 +24,26 @@ export const MENU_ITEMS: IMenuItem[] = [
   {
     icon: (
       <Icon
+        icon={faMoneyBill1Wave}
+        size='lg'
+      />
+    ),
+    label: 'Assets',
+    route: '/assets',
+  },
+  {
+    icon: (
+      <Icon
+        icon={faExchange}
+        size='lg'
+      />
+    ),
+    label: 'Transactions',
+    route: '/transactions',
+  },
+  {
+    icon: (
+      <Icon
         icon={faChartPie}
         size='lg'
       />
@@ -30,7 +51,6 @@ export const MENU_ITEMS: IMenuItem[] = [
     label: 'Chart',
     route: '/chart',
   },
-
   {
     icon: (
       <Icon
@@ -40,16 +60,6 @@ export const MENU_ITEMS: IMenuItem[] = [
     ),
     label: 'Tables',
     route: '/tables',
-  },
-  {
-    icon: (
-      <Icon
-        icon={faGear}
-        size='lg'
-      />
-    ),
-    label: 'Settings',
-    route: '/settings',
   },
   {
     icon: (
